33148fd5817176a4df32130bed72c65d5fed4dc9
Deepika411 <deepikame13@gmail.com>
2019-03-20 12:37:09 +0530

    added baseclass

---
+++ src/org/test/Test01.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Test01 {
+public static void main(String[] args) {
+	System.out.println("added baseclass");
+	
+}
+private void add(int a,float b) {
+	float c=a+b;
+	System.out.println("add nos");
+
+}
+}
